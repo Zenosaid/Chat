252d8b81dc07d8f7dd7d46276eaa8c3f3c16ab04
Zenosaid <119419666+Zenosaid@users.noreply.github.com>
2023-02-28 15:53:11 -0500

    chat

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const filteredWords = ['fuck', 'shit', 'bitch', 'dick', 'nigger', 'nigga', 'ass', 'asshole', 'whore', 'hoe']
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -11,9 +12,29 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log('a user connected');
-});
-io.on('connection', (socket) => {
-  console.log('a user connected');
+
+  socket.on('chat message', (msg) => {
+    const hasFilteredWords = filteredWords.some((word) =>
+      msg.toLowerCase().includes(word.toLowerCase())
+    );
+    // Check if the message contains any filtered words
+
+    if (!hasFilteredWords) {
+      io.emit('chat message', msg);
+    }
+  });
+
+  app.get('/chatroom', (req, res) => {
+    const joinCode = req.query.code;
+    if (joinCode) {
+      const chatRoom = chatRooms.find(room => room.code === joinCode);
+      if (chatRoom) {
+        return res.sendFile(__dirname + '/chat.html');
+      }
+    }
+    res.status(404).send('Chat room not found.');
+  });
+
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
@@ -22,17 +43,4 @@ io.on('connection', (socket) => {
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
-io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
-  });
-});
 
-io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
-
-
-io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
-});
